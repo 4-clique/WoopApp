add11d7df35ecc2d148cdc6d5476dbe5d63fb911
vddm <vddm@cin.ufpe.br>
2015-05-29 17:31:18 -0300

    Conseguiu fazer player

---
+++ app/src/main/java/br/ufpe/cin/player/MusicController.java
@@ -0,0 +1,15 @@
+package br.ufpe.cin.player;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by Simone Cohen on 29/05/2015.
+ */
+public class MusicController extends MediaController {
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+}
